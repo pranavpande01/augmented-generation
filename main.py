9ce89b0fb487f5ebc251ab745a6a311a4891109a
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-12 11:10:25 +0000

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #https://docs.llamaindex.ai/en/stable/examples/data_connectors/simple_directory_reader/
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
-
+from llama_index.core import Document
 
 
 reader=SimpleDirectoryReader(
  input_dir='data/pdfs'
 )
+
+splitter=SentenceSplitter(
+    chunk_size=512,
+    chunk_overlap=20    
+)
+
+docs=Document(text="Hello world. This is a test document. It contains multiple sentences to demonstrate the sentence splitting functionality. Each sentence will be processed accordingly.")
+nodes=splitter.get_nodes_from_documents([docs])
+print(nodes)
 
