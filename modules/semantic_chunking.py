f22878e05f0a96b60943e450af2770b444e45828
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2025-09-20 08:10:19 +0000

    first commit

--- modules/semantic_chunking.py
+++ modules/semantic_chunking.py
@@ -7,3 +7,9 @@ try:
     nltk.data.find("tokenizers/punkt")
 except:
     nltk.download("punkt",download_dir="/workspaces/augmented-generation/utils")
+
+def semantic_chunker(text:str,similarity_threshhold: float=0.82):
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    sentences=nltk.sent_tokenize(text)
+    return sentences
+
